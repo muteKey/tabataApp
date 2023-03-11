1afb9b4b3f2c1edd2f84f7f2fd135ee757910bf7
Kirill Ushkov <kirillushkov2010@gmail.com>
2023-03-11 13:10:06 +0200

    Initial Commit

---
+++ TabataApp/TabataAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TabataAppApp.swift
+//  TabataApp
+//
+//  Created by Kirill Ushkov on 11.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct TabataAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
